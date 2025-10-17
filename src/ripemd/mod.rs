9b56319d7d8cbba84c1c9a1f9195e969603a6387
lkessels <l@lmpkessels.com>
2025-10-17 11:45:29 +0200

    chore(ripemd/mod): remove pub elements that aren't used

--- src/ripemd/mod.rs
+++ src/ripemd/mod.rs
@@ -3,6 +3,6 @@ pub mod functions;
 pub mod ripemd160;
 
 pub use constants::{
-    l_round_constants, r_round_constants, LEFT_R, LEFT_S, RIGHT_R, RIGHT_S
+    l_round_constants, r_round_constants
 };
 pub use functions::{f_left, f_right};
