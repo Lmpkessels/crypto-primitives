b7a10776a8c4ce9307812a08bc48c1f4770b31c5
lkessels <l@lmpkessels.com>
2025-10-20 18:28:22 +0200

    chore(padd_pars/mod): expose padd and big endian pars project-wide

--- src/padd_pars/mod.rs
+++ src/padd_pars/mod.rs
@@ -1,9 +1,13 @@
 pub mod big_endian_padding;
-pub mod little_endian_padding;
 pub mod big_endian_parsing;
+pub mod big_endian_padding64;
+pub mod big_endian_parsing64;
+pub mod little_endian_padding;
 pub mod little_endian_parsing;
 
 pub use big_endian_padding::big_endian_padd;
 pub use big_endian_parsing::big_endian_pars;
+pub use big_endian_padding64::big_endian_padd64;
+pub use big_endian_parsing64::big_endian_pars64;
 pub use little_endian_padding::little_endian_padd;
 pub use little_endian_parsing::little_endian_pars;
